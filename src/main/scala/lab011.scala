81d39fa3e0ab625ab96bb5542aeaf9ecee9aaae8
ЧерняевАА <cernaevaa@MacBook-Pro-internet.local>
2021-03-01 18:04:43 +0300

    add rdd

--- src/main/scala/lab011.scala
+++ src/main/scala/lab011.scala
@@ -1,6 +1,7 @@
 object lab011 {
   import org.apache.spark.sql.SparkSession
 
+
   def main(args: Array[String]): Unit = {
     val appName = "lab01"
     val master = "local"
@@ -12,8 +13,15 @@ object lab011 {
       .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
     // running func
-    val uData = spark.read.textFile("/Users/cernaevaa/IdeaProjects/lab011/src/main/sources/u.data")
-    uData.show()
+    val uData = spark.read.textFile("/Users/cernaevaa/IdeaProjects/lab011/src/main/sources/u.data").rdd
+    //uData.show()
+    //uData.map(x => x.split(" "))
+    import spark.implicits._
+    uData.map(x => x.split("")).take(10).toDF()
+
+    import spark.sqlContext.implicits._
+
+    //spark.createDataFrame(uData.map(x => x.split("")).take(10))
   }
 
 
